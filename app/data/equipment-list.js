03207a441b1ef298a7e6cdca30cfd58a29e2b3d3
Luke Taylor <luke@deentaylor.com>
2019-11-19 00:30:10 -0500

    Add IDs to equipment

--- app/data/equipment-list.js
+++ app/data/equipment-list.js
@@ -2,12 +2,14 @@ module.exports.equipment = [
   // Storage
   {
     name: '32GB SD Card',
+    id: 'sd-card-32gb',
     category: { name: 'storage', id: 1, display_name: 'Digital storage' },
     total_count: 4,
     image: 'https://images.unsplash.com/photo-1499336969384-ebe67b79faa8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
   },
   {
     name: '128GB SD Card',
+    id: 'sd-card-128gb',
     category: { name: 'storage', id: 1, display_name: 'Digital storage' },
     total_count: 3,
     image: 'https://images.unsplash.com/photo-1535786647382-8345dca8e4e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
@@ -15,12 +17,14 @@ module.exports.equipment = [
   // Cameras
   {
     name: 'Canon EOS 5D',
+    id: 'canon-eos-5d',
     category: { name: 'cameras', id: 2, display_name: 'Cameras' },
     total_count: 1,
     image: 'https://www.usa.canon.com/internet/wcm/connect/us/2da6d3de-d2d6-40f7-81c2-231ddda5aa76/eos-5ds-dslr-camera-3q-lens-d.jpg?MOD=AJPERES',
   },
   {
     name: 'Camera',
+    id: 'camera-nondescript',
     category: { name: 'cameras', id: 2, display_name: 'Cameras' },
     total_count: 1,
     image: 'https://www.usa.canon.com/internet/wcm/connect/us/2da6d3de-d2d6-40f7-81c2-231ddda5aa76/eos-5ds-dslr-camera-3q-lens-d.jpg?MOD=AJPERES',
@@ -28,11 +32,13 @@ module.exports.equipment = [
   // Tripods
   {
     name: 'Tripod',
+    id: 'tripod-standard',
     category: { name: 'tripods', id: 3, display_name: 'Tripods' },
     total_count: 3,
   },
   {
     name: 'Another Tripod',
+    id: 'tripod-alternate',
     category: { name: 'tripods', id: 3, display_name: 'Tripods' },
     total_count: 1,
   },
